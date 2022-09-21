fef07514b8cf38cb15e0ab0ecc2c6ba0c0216254
Collin Abidi <cba15@pitt.edu>
2022-09-21 15:16:16 -0400

    Init commit

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -1,6 +1,8 @@
 from torchvision import datasets, transforms
 from base import BaseDataLoader
-
+from torch.utils.data import Dataset
+import torch
+import h5py
 
 class MnistDataLoader(BaseDataLoader):
     """
@@ -13,4 +15,49 @@ class MnistDataLoader(BaseDataLoader):
         ])
         self.data_dir = data_dir
         self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
+        super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+
+class H5Dataset(Dataset):
+    """
+    Credit: https://vict0rs.ch/2021/06/15/pytorch-h5/
+    """
+    def __init__(self, h5_paths, limit=-1):
+        self.limit = limit
+        self.h5_paths = h5_paths
+        self.archives = [h5py.File(h5_path, "r") for h5_path in self.h5_paths]
+        self.indices = {}
+        idx = 0
+        for a, archive in enumerate(self.archives):
+            for i in range(len(archive)):
+                self.indices[idx] = (a, i)
+        self._archives = None
+
+    @property
+    def archives(self):
+        if self._archives is None: # Lazy loading
+            self._archives = [h5py.File(h5_path, "r") for h5_path in self.h5_paths]
+        return self._archives
+    
+    def __getitem__(self, index):
+        a, i = self.indices[index]
+        archive = self.archives[a]
+        dataset = archive["data"]
+        data = torch.from_numpy(dataset[:])
+        label = archive["label"]
+        labels = torch.from_numpy(label[:])
+
+        return {"data":data, "labels":labels}
+
+    def __len__(self):
+        if self.limit > 0:
+            return min([len(self.indices), self.limit])
+        return len(self.indices)
+
+class CO2DataLoader(BaseDataLoader):
+    """
+    CO2 concentration dataset from Mauna Loa Observatory, Hawaii. Keeling et al., 2004
+    """
+    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
+        self.data_dir = data_dir
+        self.dataset = H5Dataset(data_dir)
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
--- model/model.py
+++ model/model.py
@@ -20,3 +20,51 @@ class MnistModel(BaseModel):
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
+
+class DropoutModel(BaseModel):
+    def __init__(self, p_dropout):
+        """
+        p_dropout: probability of dropout, float in range [0.0, 1.0]
+
+        'We use NNs with either 4 or 5 hidden layers and 1024 hidden units.
+        We use either ReLU non-linearities or TanH non-linearities in each
+        network, and use dropout probabilities of either 0.1 or 0.2. Exact
+        experiment setup is given in section E.1 in the appendix.'
+        https://arxiv.org/pdf/1506.02142.pdf, Section 5.1
+        """
+        super().__init__()
+        self.fc1 = nn.Linear(1, 1024)
+        self.fc2 = nn.Linear(1024, 1024)
+        self.fc3 = nn.Linear(1024, 1024)
+        self.fc4 = nn.Linear(1024, 1)
+        self.dropout = nn.Dropout(p=p_dropout)
+
+        for m in [self.fc1, self.fc2, self.fc3, self.fc4]:
+            self.initialize_weights(m)
+
+    def forward(self, x):
+        """
+        'With dropout, we sample binary variables for every input point and
+        for every network unit in each layer (apart from the last one).'
+        https://arxiv.org/pdf/1506.02142.pdf, Section 3
+        """
+        x = self.dropout(x)
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = F.relu(self.fc2(x))
+        x = self.dropout(x)
+        x = F.relu(self.fc3(x))
+        x = self.dropout(x)         
+        x = F.relu(self.fc4(x))
+
+    def initialize_weights(m):
+        """
+        'We initialize the bias at 0 and initialize the weights uniformly
+        from [-sqrt(3/fan-in), +sqrt(3/fan-in)].'
+        https://arxiv.org/pdf/1506.02157.pdf, Section E.1
+        """
+        if isinstance(m, nn.Linear):
+            nn.init.kaiming_uniform_(m.weight.data, nonlinearity='relu') # Does this do sqrt(3/fan-in)?
+            if m.bias is not None:
+                nn.init.constant_(m.bias.data, 0)
+    
